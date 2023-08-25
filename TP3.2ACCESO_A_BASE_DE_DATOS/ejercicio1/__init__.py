6ce541b9dd647f27635c105f18635f8130be802f
aurz26 <aurz2020@gmail.com>
2023-08-24 23:09:46 -0300

    CONEXION y GETS CLIENTES

--- TP3.2ACCESO_A_BASE_DE_DATOS/ejercicio1/__init__.py
+++ TP3.2ACCESO_A_BASE_DE_DATOS/ejercicio1/__init__.py
@@ -1,17 +1,49 @@
 from flask import Flask
 from config import Config
+from .database import DatabaseConnection
+
 def init_app():
     """Crea y configura la aplicacion Flask"""
     app = Flask(__name__, static_folder = Config.STATIC_FOLDER, template_folder = Config.TEMPLATE_FOLDER)
     app.config.from_object(Config)
     #Ejercio 1.1: Obtener un cliente.
-    @app.route('/customers/<int:customer_id>')
-    def cliente():            
-        return 'Bienvenidx!'
+    @app.route('/customers/<int:customer_id>', methods = ['GET'])
+    def get_cliente(customer_id):
+        query= "SELECT customer_id, first_name, last_name, email, phone, street, city, state, zip_code FROM sales.customers WHERE customer_id = %s;"
+        params = customer_id,
+        result = DatabaseConnection.fetch_one(query, params)
+        if result is not None:
+            return {
+            "id": result[0],
+            "nombre": result[1],
+            "apellido": result[2],
+            "email": result[3],
+            "telefono": result[4],
+            "calle": result[5],
+            "estado": result[6],
+            "codigo postal": result[7],
+            }, 200
+        return {"msg": "No se encontró el cliente"}, 404
+        
     #Ejercio 1.2: Obtener el listado de clientes.
     @app.route('/customers', methods = ['GET'])
-    def listado_clientes():
-        return""
+    def get_clientes():
+        query= "SELECT customer_id, first_name, last_name, email, phone, street, city, state, zip_code FROM sales.customers WHERE customer_id = %s;"
+        results = DatabaseConnection.fetch_all(query)
+        clientes = []
+        for result in results:
+            clientes.append({
+            "id": result[0],
+            "nombre": result[1],
+            "apellido": result[2],
+            "email": result[3],
+            "telefono": result[4],
+            "calle": result[5],
+            "estado": result[6],
+            "codigo postal": result[7],
+            })
+        return clientes, 200
+
     #Ejercicio 1.3: Registrar un cliente.
     @app.route('/customers', methods = ['POST'])
     def aniadir_a_lista():
